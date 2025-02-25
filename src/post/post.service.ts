57c70197df6bedc59641f4e01bb7b6773851ff97
jimini <jiminido1@gmail.com>
2025-02-25 14:06:29 +0900

    trying to make structure

---
+++ src/post/post.service.ts
@@ -0,0 +1,33 @@
+import { Post } from "./post.entity";
+import { PostRepository } from "./post.repository";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class PostService {
+    constructor(private readonly postRepository: PostRepository) {}
+
+    async findAll(): Promise<Post[]> {
+        return this.postRepository.find();
+    }
+
+    async findOne(id: number): Promise<Post> {
+        return this.postRepository.findOne({where: {id}});
+    }
+
+    async create(createPostDto: CreatePostDto): Promise<Post> {
+        const post = this.postRepository.create(createPostDto);
+        return this.postRepository.save(post);
+    }
+
+    async update(id: number, updatePostDto: UpdatePostDto): Promise<Post | null> {
+        await this.postRepository.update(id, updatePostDto);
+        return this.postRepository.findOne({where: {id}});
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.postRepository.delete(id);
+    }
+    
+}
