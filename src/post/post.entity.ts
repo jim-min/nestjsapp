3ff2dddb67f3504f4084c14b5fd9f9e50398afe7
jimini <jiminido1@gmail.com>
2025-02-27 14:11:42 +0900

    entity

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,6 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+  name: 'posts'
+})
 export class Post {
-    id: number;
-    title: string;
-    content: string;
-    authorId: number;
+  @PrimaryGeneratedColumn({
+    type: 'int',
+  })
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false
+  })
+  title: string;
+
+  @Column({
+    type: 'text',
+    nullable: false
+  })
+  content: string;
+  
+  @Column({
+    type: 'int',
+    nullable: false,
+    name: 'author_id'
+  })
+  authorId: number;
   }
