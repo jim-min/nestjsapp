381a762a10c5a4e9b32ecf13556cbdf0b0ae7e2e
jimini <jiminido1@gmail.com>
2025-03-15 18:24:49 +0900

    imports added

--- src/bucket-list-items/bucket-list-items.module.ts
+++ src/bucket-list-items/bucket-list-items.module.ts
@@ -6,10 +6,12 @@ import { BucketList } from 'src/bucket-lists/entities/bucket-list.entity';
 import { BucketListItem } from './entities/bucket-list-item.entity';
 import { BucketListItemsService } from './bucket-list-items.service';
 import { Destination } from 'src/destinations/entities/destination.entity';
+import { BucketListsModule } from 'src/bucket-lists/bucket-lists.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, BucketList, BucketListItem, Destination])
+    TypeOrmModule.forFeature([User, BucketList, BucketListItem, Destination]),
+    BucketListsModule
   ],
   providers: [BucketListItemsService],
   controllers: [BucketListItemsController]
